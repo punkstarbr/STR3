b607e9dbd1312adcddfb46ec00b4fdb03b740930
punkstarbr <130254517+punkstarbr@users.noreply.github.com>
2023-04-14 04:31:03 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -2,10 +2,7 @@ import youtube_dl
 
 # Define options for youtube-dl
 ydl_opts = {
-    'format': 'best',  # Selects the best quality available
-    'outtmpl': '%(title)s.%(ext)s',  # Output filename template
     'write_all_thumbnails': False,  # Don't download thumbnails
-    'skip_download': False,  # Download the video
 }
 
 # Create a YoutubeDL object with the options
@@ -18,12 +15,5 @@ try:
         formats = info['formats']
         for f in formats:
             print(f['format_id'], f['ext'], f['format_note'])
-except youtube_dl.DownloadError as e:
-    print(f"youtube-dl failed for link https://vimeo.com/817420828: {e}")
-
-# Download the video
-try:
-    with ydl:
-        ydl.download(['https://vimeo.com/817420828'])
 except youtube_dl.DownloadError as e:
     print(f"youtube-dl failed for link https://vimeo.com/817420828: {e}")
