bf438c0b083e91ca57ca22103d9611e11ef09a42
punkstarbr <130254517+punkstarbr@users.noreply.github.com>
2023-04-14 07:22:23 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,79 +1,68 @@
-import requests
 import subprocess
+import os
+import yt_dlp
+import youtube_dl
 
-def is_channel_working(url, headers=None):
-    try:
-        cmd = [
-            "ffmpeg",
-            "-headers",
-            f"User-Agent: {headers['User-Agent']}" if headers and "User-Agent" in headers else "User-Agent: python-requests/2.25.1",
-            "-i",
-            url,
-            "-t",
-            "10",
-            "-f",
-            "null",
-            "-"
-        ]
+# Read the links from the YOUTUBEALL.txt file
+try:
+    with open('YOUTUBEALL.txt', 'r') as f:
+        links = [line.strip() for line in f.readlines()]
+except Exception as e:
+    print(f"Error reading the YOUTUBEALL.txt file: {e}")
+    links = []
 
-        process = subprocess.run(cmd, stderr=subprocess.PIPE, universal_newlines=True, timeout=5)
-        return process.returncode == 0
-    except (subprocess.TimeoutExpired, subprocess.CalledProcessError):
-        return False
+banner = r'''
+#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"
+#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"
+#EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"
+#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"
+'''
 
-repo_urls = [
-    "https://github.com/LITUATUI/M3UPT/raw/main/M3U/M3UPT.m3u"
-]
+# Install yt-dlp and youtube-dl
+subprocess.run(['pip', 'install', '--upgrade', 'yt-dlp'])
+subprocess.run(['pip', 'install', '--upgrade', 'youtube_dl'])
 
-working_channels = []
+# Define options for yt-dlp and youtube-dl
+ydl_opts = {
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',  # Get the best quality in mp4 format
+    'format': 'best',  # Obtém a melhor qualidade
 
-for url in repo_urls:
-    m3u_response = requests.get(url)
 
-    if m3u_response.status_code == 200:
-        m3u_lines = m3u_response.text.splitlines()
+    'write_all_thumbnails': False,  # Don't download thumbnails
+    'skip_download': True,  # Don't download the video
+    'write_all_thumbnails': False,  # Não faz download das thumbnails
+    'skip_download': True,  # Não faz download do vídeo
+}
 
-        for i, line in enumerate(m3u_lines):
-            if line.startswith("#EXTM3U"):
-                working_channels.append({"extm3u_line": line})
-            elif line.startswith("#EXTINF"):
-                extinf_line = line
-                extvlcopt_line = None
-                kodiprop_lines = []
-                stream_url = None
-                headers = {}
 
-                for j in range(i + 1, len(m3u_lines)):
-                    next_line = m3u_lines[j]
-                    
-                    if next_line.startswith("#EXTVLCOPT"):
-                        extvlcopt_line = next_line
-                        if "http-user-agent" in extvlcopt_line:
-                            user_agent = extvlcopt_line.split("=")[1]
-                            headers["User-Agent"] = user_agent
-                    elif next_line.startswith("#KODIPROP"):
-                        kodiprop_lines.append(next_line)
-                    elif not next_line.startswith("#"):
-                        stream_url = next_line
-                        break
+# Get the playlist and write to file
+try:
+    with open('./LISTA5YTALL.m3u', 'w', encoding='utf-8') as f:
+        f.write("#EXTM3U\n")
+        f.write(banner)
+        for i, link in enumerate(links):
+            try:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(link, download=False)
+            except Exception as e:
+                print(f"yt-dlp failed for link {link}: {e}")
+                print("Trying with youtube-dl...")
+                try:
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl_backup:
+                        info = ydl_backup.extract_info(link, download=False)
+                except Exception as e_backup:
+                    print(f"youtube-dl failed for link {link}: {e_backup}")
+                    continue
 
-                if stream_url:
-                    if is_channel_working(stream_url, headers):
-                        working_channels.append({
-                            "extinf_line": extinf_line,
-                            "extvlcopt_line": extvlcopt_line,
-                            "kodiprop_lines": kodiprop_lines,
-                            "stream_url": stream_url
-                        })
-
-with open("lista3.M3U", "w") as f:
-    for channel in working_channels:
-        if "extm3u_line" in channel:
-            f.write(f"{channel['extm3u_line']}\n")
-        else:
-            f.write(f"{channel['extinf_line']}\n")
-            if channel['extvlcopt_line']:
-                f.write(f"{channel['extvlcopt_line']}\n")
-            for kodiprop_line in channel['kodiprop_lines']:
-                f.write(f"{kodiprop_line}\n")
-            f.write(f"{channel['stream_url']}\n")
+            if 'url' not in info:
+                print(f"Error writing video information for {link}: 'url'")
+                continue
+            url = info['url']
+            thumbnail_url = info['thumbnail']
+            description = info.get('description', '')[:10]
+            title = info.get('title', '')
+            f.write(f"#EXTINF:-1 group-title=\"YOUTUBE\"  tvg-logo=\"{thumbnail_url}\",{title} - {description}...\n")
+            f.write(f"{url}\n")
+            f.write("\n")
+except Exception as e:
+    print(f"Error creating the .m3u file: {e}")
