a4b5a9042aed1c36d08fe885be47fbc239c2560b
punkstarbr <130254517+punkstarbr@users.noreply.github.com>
2023-04-14 03:02:58 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,12 +1,17 @@
 import subprocess
 
 url = "https://www.bilibili.com/video/BV1qE411o7Zs/?spm_id_from=333.788.recommend_more_video.11"
-command = ["youtube-dl", "-s", "--get-title", "--no-warnings", url]
+command = ["youtube-dl", "-s", "--dump-json", url]
 result = subprocess.check_output(command, universal_newlines=True)
 
-# extrair o título do vídeo
-title = result.strip()
+# extrair o título e a duração do vídeo
+for line in result.split("\n"):
+    if line.strip():
+        data = eval(line.strip())
+        title = data["title"]
+        duration = data["duration"]
+        break
 
 # salvar informações em formato EXTINF
 with open("BILIBILI.m3u", "w") as f:
-    f.write(f"#EXTINF:-1,{title}\n{url}")
+    f.write(f"#EXTINF:{duration},{title}\n{url}")
