db6d6a2be4098756b5936264f72a52efd9526d0c
punkstarbr <130254517+punkstarbr@users.noreply.github.com>
2023-04-16 23:20:26 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -201,4 +201,79 @@ try:
 except Exception as e:
     print(f"Erro ao criar o arquivo .m3u: {e}")
 
+import subprocess
+import time
+import os
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import yt_dlp
+
+from pytube import YouTube
+
+def generate_playlist(url_youtube):
+    # Configuring Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+
+    # Instanciando o driver do Chrome
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Abrir a página desejada
+    driver.get(url_youtube)
+
+    # Aguardar alguns segundos para carregar todo o conteúdo da página
+    time.sleep(5)
+
+    # Get the page source again after scrolling to the bottom
+    html_content = driver.page_source
+
+    time.sleep(5)
+
+    # Find the links and titles of the videos found
+    try:
+        soup = BeautifulSoup(html_content, "html.parser")
+        videos = soup.find_all("a", id="video-title", class_="yt-simple-endpoint style-scope ytd-video-renderer")
+        links = ["https://www.youtube.com" + video.get("href") for video in videos]
+        titles = [video.get("title") for video in videos]
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        # Close the driver
+        driver.quit()
+
+    # Define as opções para o youtube-dl
+    ydl_opts = {
+        'format': 'best',  # Obtém a melhor qualidade
+        'write_all_thumbnails': False,  # Não faz download das thumbnails
+        'skip_download': True,  # Não faz download do vídeo
+    }
+
+    # Get the playlist and write to file
+    try:
+        with open('./LISTA5YTALL.m3u', 'a', encoding='utf-8') as f:
+            f.write("#EXTM3U\n")
+            for i, link in enumerate(links):
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(link, download=False)
+                if 'url' not in info:
+                    print(f"Erro ao gravar informações do vídeo {link}: 'url'")
+                    continue
+                url = info['url']
+                thumbnail_url = info['thumbnail']
+                description = info.get('description', '')[:10]
+                title = info.get('title', '')
+                f.write(f"#EXTINF:-1 group-title=\"USA NEWS\" tvg-logo=\"{thumbnail_url}\",{title} - {description}...\n")
+                f.write(f"{url}\n")
+                f.write("\n")
+    except Exception as e:
+        print(f"Erro ao criar o arquivo .m3u8: {e}")
+
+url_youtube1 = "https://www.youtube.com/results?search_query=telemundo+noticiero&sp=CAISBhABGAIgAQ%253D%253D"
+url_youtube2 = "https://www.youtube.com/results?search_query=univision+vivo&sp=CAISAhAB"
+
+generate_playlist(url_youtube1)
+generate_playlist(url_youtube2)
 
