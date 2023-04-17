7754a17420f5c5efa78c0566d934f0df00b8de4a
punkstarbr <130254517+punkstarbr@users.noreply.github.com>
2023-04-17 06:04:32 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,20 +1,28 @@
 import youtube_dl
 
-# Defina a URL do vídeo
-video_url = 'https://vimeo.com/818191556'
+# Crie uma lista com as URLs dos canais
+channels = [
+    'https://vimeo.com/user125724700',
+    'https://vimeo.com/user191718673',
+    'https://vimeo.com/poupainvest',
+    'https://vimeo.com/user132917313'
+]
 
-# Defina o nome do arquivo de saída
-output_file = 'AMOR.m3u'
+# Defina a pasta de destino
+dest_folder = '/content/drive/MyDrive/HOJE/QUINTA/'
 
 # Defina as opções para o youtube_dl
 ydl_opts = {
     'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-    'outtmpl': output_file,
-    'forcefilename': True
+    'outtmpl': dest_folder,
 }
 
 # Crie o objeto youtube_dl
 ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+# Percorra cada URL do canal e baixe os vídeos
+for channel in channels:
+    ydl.download([channel])
 
 # Baixe as informações do vídeo e salve em um arquivo M3U
 info_dict = ydl.extract_info(video_url, download=False)
